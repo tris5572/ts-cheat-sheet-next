0998f1ed246b14d9e2ab0fff6d83274c10089b51
tris <nexus4fortune@gmail.com>
2023-06-24 16:15:21 +0900

    幅固定とデータ追加

--- src/data/data.ts
+++ src/data/data.ts
@@ -91,7 +91,8 @@ for (const v of Object.values(o)) {
     code: `
 const n1: number = 42;
 const n2: number = 3.1415;
-const s: string = "text";
+const s: string = "text"; // 文字と文字列は同じ
+const b: boolean = true; // or false
 const a: number[] = [1, 2, 3];
 const o1: { a: number; b: number } = { a: 1, b: 2 };
 const o2: { [keys: string]: number } = { "1": 1, あ: 2 };
@@ -149,7 +150,7 @@ Math.sign(-3); // -1
   },
   {
     category: "Math",
-    name: "数値比較",
+    name: "数値の大小比較",
     code: `
 Math.min(-1, 0, 2); // -1
 Math.max(-1, 0, 2); // 2
@@ -184,6 +185,20 @@ String(12).padStart(4, "0"); // 0012
 `,
   },
   {
+    category: "String",
+    name: "文字列の出現回数を調べる",
+    code: `
+const s = "abcaA";
+// 正規表現直書き版
+(s.match(/a/g) || []).length; // 2
+(s.match(/z/g) || []).length; // 0
+// 変数で調べる場合は RegExp を使う
+const c = "a";
+(s.match(new RegExp(c, "g")) || []).length; // 2
+(s.match(new RegExp(c, "gi")) || []).length; // 3
+`,
+  },
+  {
     category: "Data",
     name: "配列の分割代入",
     code: `
@@ -213,7 +228,104 @@ a.unshift(-2, -1); // [-2, -1, 0, 1]
 // 先頭から取り除く
 a.shift(); // 戻り値 -2、配列 [-1, 0, 1]
 a.shift(); // 戻り値 -1、配列 [0, 1]
+`,
+  },
+  {
+    category: "Data",
+    name: "配列の一部を取得",
+    code: `
+const a = [0, 1, 2, 3, 4];
 
+// 開始から末尾(含まない)までを取得
+a.slice(0, 2); // [0, 1]
+a.slice(1, 4); // [1, 2, 3]
+
+// 末尾を省略したときは最後まで
+a.slice(2); // [2, 3, 4]
+`,
+  },
+  {
+    category: "Data",
+    name: "配列の要素を削除して取り出す",
+    code: `
+// 指定した位置以降の要素を削除する
+const a = [0, 1, 2, 3, 4];
+const b = a.splice(3);
+console.log(a); // [0, 1, 2]
+console.log(b); // [3, 4]
+
+// 指定位置から指定数の要素を削除する
+const c = [0, 1, 2, 3, 4];
+const d = c.splice(1, 2);
+console.log(c); // [0, 3, 4]
+console.log(d); // [1, 2]
+
+// 指定位置から指定数の要素を削除して要素を追加する
+const e = [0, 1, 2, 3, 4];
+const f = e.splice(1, 2, 97, 98, 99);
+console.log(e); // [0, 97, 98, 99, 3, 4]
+console.log(f); // [1, 2]
+`,
+  },
+  {
+    category: "Data",
+    name: "配列のソート (number)",
+    code: `
+const a = [10, 1, 5];
+
+// デフォルトでは文字列としてソート
+a.sort(); // [1, 10, 5]
+
+// 昇順ソート
+a.sort((a, b) => a - b); // [1, 5, 10]
+
+// 降順ソート
+a.sort((a, b) => b - a); // [10, 5, 1]
+`,
+  },
+  {
+    category: "Data",
+    name: "配列から条件に一致したものを取り出す",
+    code: `
+const a = [1, 2, 3, 4, 5, 6];
+
+const v = a.filter((v) => v % 2 === 0); // [2, 4, 6]
+const l = a.filter((v) => v % 2 === 0).length; // 3
+`,
+  },
+  {
+    category: "Data",
+    name: "配列を文字列化",
+    code: `
+const a = [1, 2, 3];
+
+// 何も指定しないときはカンマ区切り
+a.join(); // 1,2,3
+
+// 区切り文字列を指定
+a.join(" "); // 1 2 3
+a.join(" - "); // 1 - 2 - 3
+a.join(""); // 123
+`,
+  },
+  {
+    category: "Data",
+    name: "重複を許さないSet",
+    code: `
+// 重複したものは追加されない
+// 追加された順番を維持する
+const s = new Set<number>();
+s.add(3); // 3
+s.add(1); // 3, 1
+s.add(3); // 3, 1
+s.add(2); // 3, 1, 2
+console.log(s.size); // 3
+
+// Set を Array に変換
+const a = Array.from(s); // [3, 1, 2]
+
+// Array を Set に変換
+const s2 = new Set([4, 6, 5]); // 4, 6, 5
 `,
   },
 ];
